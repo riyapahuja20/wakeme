b71506e8395e232cae072f76168560e4edfe7864
riyapahuja20 <riyapahuja20@yahoo.com>
2017-11-23 10:21:41 +0530

    First Push

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
+import { Geolocation } from '@ionic-native/geolocation';
+import { NativeGeocoder } from '@ionic-native/native-geocoder';
+//import { LocationAccuracy } from '@ionic-native/location-accuracy';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    ListPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    ListPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    Geolocation,
+    NativeGeocoder,
+    //LocationAccuracy,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/home/home.ts
@@ -0,0 +1,95 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController, ToastController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+import { NativeGeocoder, NativeGeocoderReverseResult } from '@ionic-native/native-geocoder';
+import { LocalNotifications } from '@ionic-native/local-notifications';
+
+declare var google;
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+	@ViewChild('map') mapElement: ElementRef;
+  	map: any;
+
+  constructor(public navCtrl: NavController, public geolocation: Geolocation,public geocoder: NativeGeocoder, public toaster: ToastController) {
+
+  }
+
+  ionViewDidLoad(){
+    this.loadMap();
+  }
+
+  addMarker(){
+ 
+  let marker = new google.maps.Marker({
+    map: this.map,
+    animation: google.maps.Animation.DROP,
+    position: this.map.getCenter()
+  });
+ 
+  // this.getAlarm();
+  // let content = "<h4>Information!</h4>";          
+ 
+  // this.addInfoWindow(marker, content);
+ 
+}
+
+// public getAlarm() {
+//         this.localNotifications.schedule({
+//             title: "Test Title",
+//             text: "Delayed Notification",
+//             sound: 'file://abc4.mp3',
+//             at: new Date(new Date().getTime() + 5 * 1000)
+            
+//         });
+//     }
+ 
+  loadMap(){
+ 
+    //let latLng = new google.maps.LatLng(-34.9290, 138.6010);
+
+    this.geolocation.getCurrentPosition().then((position) => {
+ 
+ 	console.log(position);
+ 	
+    let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
+ 
+    let mapOptions = {
+      enableHighAccuracy: true,
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+ 
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+    //this.getcountry(position);
+ 
+  }, (err) =>{
+  	console.log(err);
+  });
+
+    let watch = this.geolocation.watchPosition();
+		watch.subscribe((position) => {
+  			console.log(position.coords.longitude + ' ' + position.coords.latitude);
+		// data can be a set of coordinates, or an error (if an error occurred).
+		// data.coords.latitude
+		// data.coords.longitude
+
+		
+		});
+
+}
+getcountry(pos){
+	this.geocoder.reverseGeocode(pos.coords.latitude, pos.coords.longitude).then((res: NativeGeocoderReverseResult) => {
+      let country = this.toaster.create({
+        message: res.countryName,
+        duration: 4000
+      });
+      country.present();
+    })
+}
+}
